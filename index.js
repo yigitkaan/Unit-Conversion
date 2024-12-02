d5ce2871e20a83a398e1ad0e8dd7f0595b1e46d4
Yiğit Kaan Topcu <65395094+yigitkaan@users.noreply.github.com>
2024-12-02 18:08:07 +0300

    Düzenlemeler yapıldı

--- index.js
+++ index.js
@@ -11,24 +11,44 @@ const liter = 0.264;
 const kilogram = 2.204;
 
 btnEl.addEventListener("click", function () {
-  let value = Number(inputEl.value);
+  let value = inputEl.value;
   let txt = ``;
 
-  for (let i = 0; i < resultsArr.length; i++) {
-    if (i === 0) {
-      txt = `${value} meters = ${(value * meter).toFixed(
-        2
-      )} feet | ${value} feet = ${(value / meter).toFixed(2)} meters`;
-    } else if (i === 1) {
-      txt = `${value} liters = ${(value * liter).toFixed(
-        2
-      )} gallons | ${value} gallons = ${(value / liter).toFixed(2)} liters`;
-    } else {
-      txt = `${value} kilos = ${(value * kilogram).toFixed(
-        2
-      )} pounds | ${value} pounds = ${(value / kilogram).toFixed(2)} kilos`;
-    }
+  conversionCalculator(value);
+});
 
-    resultsArr[i].textContent = txt;
+inputEl.addEventListener("keydown", function () {
+  if (event.key === "Enter") {
+    let value = inputEl.value;
+    let txt = ``;
+
+    conversionCalculator(value);
   }
 });
+
+function conversionCalculator(value) {
+  if (!isNaN(value)) {
+    for (let i = 0; i < resultsArr.length; i++) {
+      if (i === 0) {
+        txt = `${value} meters = ${(value * meter).toFixed(
+          2
+        )} feet | ${value} feet = ${(value / meter).toFixed(2)} meters`;
+      } else if (i === 1) {
+        txt = `${value} liters = ${(value * liter).toFixed(
+          2
+        )} gallons | ${value} gallons = ${(value / liter).toFixed(2)} liters`;
+      } else {
+        txt = `${value} kilos = ${(value * kilogram).toFixed(
+          2
+        )} pounds | ${value} pounds = ${(value / kilogram).toFixed(2)} kilos`;
+      }
+
+      resultsArr[i].textContent = txt;
+    }
+  } else {
+    alert("Please enter a number!");
+  }
+
+  value = "";
+  inputEl.value = value;
+}
